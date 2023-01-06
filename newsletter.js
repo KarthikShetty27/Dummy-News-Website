68d441018b311d77f2ead1b4e60fa09583808dba
Karthik Shetty <91489581+KarthikShetty27@users.noreply.github.com>
2023-01-06 18:22:43 +0530

    Delete newsletter.js

--- newsletter.js
+++
@@ -1,8 +0,0 @@
-const email = document.getElementById('email');
-const Submit = document.getElementById('Submit');
-const output = document.getElementById('output');
-
-function display(){
-    alert(email.value+'\n'+' Thanks for Subscribing to The Daily Bugle !! ');
-}
-Submit.addEventListener('click',display);
